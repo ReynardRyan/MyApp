da65ab80b254782a4688cee95266f98c664f34b0
ReynardRyan <reynardryan8@gmail.com>
2025-10-05 02:42:20 +0700

    feat : user information and logout on home page

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,106 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   View,
   Text,
+  StyleSheet,
+  Alert,
 } from 'react-native';
+import { Button } from '../components';
+import { colors } from '../utils';
+import { useAuthStore } from '../stores/authStore';
 
 const HomeScreen = () => {
+  const { userEmail, userToken, logout } = useAuthStore();
+
+  const handleLogout = () => {
+    Alert.alert(
+      'Logout',
+      'Are you sure?',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        {
+          text: 'Logout',
+          style: 'destructive',
+          onPress: async () => {
+            try {
+              await logout();
+            } catch (error) {
+              console.error('Error logout:', error);
+              Alert.alert('Error', 'Failed to logout');
+            }
+          },
+        },
+      ]
+    );
+  };
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Welcome Home!</Text>
+      </View>
+
+      <View style={styles.userInfo}>
+        <Text style={styles.infoTitle}>User Information:</Text>
+        <Text style={styles.infoText}>Email: {userEmail || 'No email found'}</Text>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Logout"
+          onPress={handleLogout}
+          variant="outline"
+        />
+      </View>
     </View>
   )
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    padding: 20,
+  },
+  header: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: colors.textPrimary,
+    marginBottom: 8,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: colors.textSecondary,
+  },
+  userInfo: {
+    backgroundColor: colors.surface,
+    padding: 20,
+    borderRadius: 12,
+    marginBottom: 30,
+  },
+  infoTitle: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: colors.textPrimary,
+    marginBottom: 12,
+  },
+  infoText: {
+    fontSize: 14,
+    color: colors.textSecondary,
+    marginBottom: 8,
+  },
+  buttonContainer: {
+    marginTop: 'auto',
+    // marginBottom: 50,
+  },
+});
 
 
 export default HomeScreen;
